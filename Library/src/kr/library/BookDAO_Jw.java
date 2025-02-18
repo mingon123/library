30714e1e2f14f19fe07876edef38889e596f5e98
home <home@DESKTOP-9V8FFU6>
2025-02-19 01:33:43 +0900

    구현 진행 중_25.02.18

--- Library/src/kr/library/BookDAO_Jw.java
+++ Library/src/kr/library/BookDAO_Jw.java
@@ -15,20 +15,20 @@ public class BookDAO_Jw {
 		PreparedStatement pstmt = null;
 		String sql = null;
 		int cnt = 0;
-		
+
 		try {
 			conn = DBUtil.getConnection();
 			sql = "INSERT INTO book VALUES (book_seq.nextval,?,?,NVL(?,''),?,NVL(?,'기타'),?,?,SYSDATE)";
 			pstmt = conn.prepareStatement(sql);
-			
+
 			pstmt.setString(++cnt, title);
 			pstmt.setString(++cnt, author);
 			pstmt.setString(++cnt, publisher);
 			pstmt.setObject(++cnt, (publication_year != null) ? publication_year : 0, Types.INTEGER);
 			pstmt.setString(++cnt, category);
-	        pstmt.setObject(++cnt, (rank != null) ? rank : 0, Types.INTEGER);
-	        pstmt.setObject(++cnt, (volm_cnt != null) ? volm_cnt : 0, Types.INTEGER);
-		
+			pstmt.setObject(++cnt, (rank != null) ? rank : 0, Types.INTEGER);
+			pstmt.setObject(++cnt, (volm_cnt != null) ? volm_cnt : 0, Types.INTEGER);
+
 			int count = pstmt.executeUpdate();
 			System.out.println(count + "개의 행을 삽입했습니다.");
 		} catch (Exception e) {
@@ -37,32 +37,30 @@ public class BookDAO_Jw {
 			DBUtil.executeClose(null, pstmt, conn);
 		}
 	} // insertInfo
-	
+
 	//랜덤 책 정보 - 숫자 입력받아 하나로 재사용
-	public void randomBookInfo(int num) {
+	public void randomBookInfo(int num) { // 평점 정보 출력하게 추가?
 		Connection conn = null;
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
 		String sql = null;
 		HashSet<Integer> hs = new HashSet<Integer>();
 		int bookLength = 0;
-		
+
 		try {
-			
 			conn = DBUtil.getConnection();
 			sql = "SELECT COUNT(*) FROM book";
 			pstmt = conn.prepareStatement(sql);
 			rs = pstmt.executeQuery();
-			
 			if(rs.next()) bookLength = rs.getInt(1);
-			
+
 			while(hs.size() < num ) {
 				hs.add((int) (Math.random() * bookLength) + 1);
 			}
-			
+
 			sql = "SELECT * FROM book WHERE BOOK_NUM=?";
 			pstmt = conn.prepareStatement(sql);
-			
+
 			for (int randNum : hs) {
 				pstmt.setInt(1, randNum);
 				rs = pstmt.executeQuery();
@@ -71,42 +69,164 @@ public class BookDAO_Jw {
 					System.out.println("책번호 : " + rs.getInt("BOOK_NUM"));
 					System.out.println("제목 : " + rs.getString("BOOK_TITLE"));
 					System.out.println("저자 : " + rs.getString("BOOK_AUTHOR"));
-					
+
 					String publisher = rs.getString("BOOK_PUBLISHER");
 					if(publisher == null) publisher = "";
 					System.out.println("출판사 : " + publisher);
-					
+
 					String publication_year = rs.getString("BOOK_P_YEAR");
 					if(publication_year == null) publication_year = "";
 					System.out.println("출판년도 : " + publication_year);
-					
+
 					String category = rs.getString("BOOK_CATEGORY");
 					if(category == null) category = "";
 					System.out.println("카테고리 : " + category);
-					
+
 					String rank = rs.getString("BOOK_RANK");
 					if(rank == null) rank = "";
 					System.out.println("추천순위 : " + rank);
-					
-					
+
 					String volm_cnt = rs.getString("BOOK_VOLM_CNT");
 					if(volm_cnt == null) volm_cnt = "";
 					System.out.println("남은 권수 : " + volm_cnt);
 					System.out.println("등록일 : " + rs.getDate("BOOK_REG_DATE"));
-					
 				} else {
 					System.out.println("오류발생");
 				}
 			} // for randNum
-			
-			
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
 			DBUtil.executeClose(null, pstmt, conn);
 		}
 	} // randomBookInfo
-	
+
+	// 대여가능 여부 판별 함수. 대여가능:1 책남아있는 권수0:0 대여권수 다 참:-1
+	public int canOrder(String mem_id,int book_num) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String sql = null;
+		int bookCount = -1;
+		int orderCount = -1;
+		try {
+			conn = DBUtil.getConnection();
+			sql = "SELECT BOOK_VOLM_CNT FROM book WHERE BOOK_NUM=?";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, book_num);
+			rs = pstmt.executeQuery();
+
+			if(rs.next()) bookCount = rs.getInt(1);
+
+			sql = "SELECT COUNT(*) FROM book_order WHERE mem_id=?";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, mem_id);
+			rs = pstmt.executeQuery();
+
+			if(rs.next()) orderCount = rs.getInt(1);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			DBUtil.executeClose(rs, pstmt, conn);
+		}
+		//테스트용. 잘되면 이 부분 지우기
+		if(bookCount == -1 || orderCount == -1)
+			System.out.println("에러발생!! 대여여부 판별 불가");
+
+		if(bookCount > 0) {
+			if(orderCount < 3) {
+				return 1;
+			}else return -1;
+		}else return 0;
+	}//canOrder
+
+	// 책번호가 존재하는지 확인하는 함수- 존재:1 존재x:0 에러:-1
+	public int checkBookNum(int book_num) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String sql = null;
+		int check = -1;
+		try {
+			conn = DBUtil.getConnection();
+			sql = "SELECT COUNT(*) FROM book WHERE BOOK_NUM=?";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, book_num);
+			rs = pstmt.executeQuery();
+
+			if(rs.next()) check = rs.getInt(1);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			DBUtil.executeClose(rs, pstmt, conn);
+		}
+
+		if(check == -1)
+			System.out.println("에러 발생! 책 번호 있는지 확인 불가");
+
+		return check;
+	}//checkBookNum
+
+	// book_order 테이블에 insert - 대여 테이블 추가후 책 갯수 조정
+	public void insertBookOrder(String mem_id,int book_num) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		String sql = null;
+		try {
+			conn = DBUtil.getConnection();
+			sql = "INSERT INTO book_order (ORDER_NUM, MEM_ID, BOOK_NUM)VALUES(book_order_seq.nextval,?,?)";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, mem_id);
+			pstmt.setInt(2, book_num);
+			int count = pstmt.executeUpdate();
+
+			sql = "UPDATE book SET book_volm_cnt = (SELECT book_volm_cnt FROM book WHERE book_num = ?) - 1 WHERE book_num = ?";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, book_num);
+			pstmt.setInt(2, book_num);
+			int count2 = pstmt.executeUpdate();
+
+			if(count > 0 && count2 > 0 ) {
+				System.out.println(book_num+"번책 대여 성공!");
+			}
+			else {
+				System.out.println("대여 중 오류발생!");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			DBUtil.executeClose(null, pstmt, conn);
+		}
+	} // insertBookOrder
+
+	// reservation 테이블에 insert - 예약 테이블에 insert
+	public void insertReserve(String mem_id,int book_num) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		String sql = null;
+		try {
+			conn = DBUtil.getConnection();
+			sql = "INSERT INTO reservation VALUES(reservation_seq.nextval,?,?)";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, mem_id);
+			pstmt.setInt(2, book_num);
+			int count = pstmt.executeUpdate();
+
+			if(count > 0) {
+				System.out.println(book_num+"번책 예약 성공!");
+			}
+			else {
+				System.out.println("예약 중 오류발생!");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			DBUtil.executeClose(null, pstmt, conn);
+		}
+	} // insertBookOrder
+
 	// 목록보기
 	public void selectInfo() {
 		Connection conn = null;
@@ -119,7 +239,7 @@ public class BookDAO_Jw {
 			pstmt = conn.prepareStatement(sql);
 			rs = pstmt.executeQuery();
 			System.out.println("-".repeat(50));
-			
+
 			if(rs.next()) {
 				System.out.println("번호\t제목\t저자\t카테고리\t출판년도");
 				do {
@@ -142,7 +262,7 @@ public class BookDAO_Jw {
 			DBUtil.executeClose(rs, pstmt, conn);
 		} 
 	} // selectInfo
-	
+
 	// 상세정보보기
 	public void selectDetailInfo(int num) {
 		Connection conn = null;
@@ -155,24 +275,24 @@ public class BookDAO_Jw {
 			pstmt = conn.prepareStatement(sql);
 			pstmt.setInt(1, num);
 			rs = pstmt.executeQuery();
-			
+
 			if(rs.next()) {
 				System.out.println("책번호 : " + rs.getInt("num"));
 				System.out.println("제목 : " + rs.getString("title"));
 				System.out.println("저자 : " + rs.getString("author"));
-				
+
 				String publisher = rs.getString("publisher");
 				if(publisher == null) publisher = "";
 				System.out.println("출판사 : " + publisher);
-				
+
 				String publication_year = rs.getString("publication_year");
 				if(publication_year == null) publication_year = "";
 				System.out.println("출판년도 : " + publication_year);
-				
+
 				String category = rs.getString("category");
 				if(category == null) category = "";
 				System.out.println("카테고리 : " + category);
-				
+
 				String rank = rs.getString("rank");
 				if(rank == null) rank = "";
 				System.out.println("추천순위 : " + rank);
@@ -190,29 +310,7 @@ public class BookDAO_Jw {
 			DBUtil.executeClose(null, pstmt, conn);
 		}
 	} // selectDetailInfo
-	
-	// 조회하는 레코드 존재 여부 체크
-	public int checkRecord(int num) {
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
-		String sql = null;
-		int count = 0;
-		try {
-			conn = DBUtil.getConnection();
-			sql = "SELECT * FROM book WHERE num=?";
-			pstmt = conn.prepareStatement(sql);
-			pstmt.setInt(1, num);
-			rs = pstmt.executeQuery();
-			if(rs.next()) count = 1;
-		} catch (Exception e) {
-			count = -1;
-		} finally {
-			DBUtil.executeClose(rs, pstmt, conn);
-		}
-		return count;
-	} // checkRecord
-	
+
 	// update
 	public void updateInfo(int num,String title,String author,String publisher,Integer publication_year,String category,Integer rank,Integer volm_cnt) {
 		Connection conn = null;
@@ -223,16 +321,16 @@ public class BookDAO_Jw {
 			conn = DBUtil.getConnection();
 			sql = "UPDATE book SET title=?,author=?,publisher=?,publication_year=?,category=NVL(?,'기타'),rank=?,volm_cnt=? WHERE num=?";
 			pstmt = conn.prepareStatement(sql);
-			
+
 			pstmt.setString(++cnt, title);
 			pstmt.setString(++cnt, author);
 			pstmt.setString(++cnt, publisher);
 			pstmt.setObject(++cnt, (publication_year != null) ? publication_year : 0, Types.INTEGER);
 			pstmt.setString(++cnt, category);
-	        pstmt.setObject(++cnt, (rank != null) ? rank : 0, Types.INTEGER);
-	        pstmt.setObject(++cnt, (volm_cnt != null) ? volm_cnt : 0, Types.INTEGER);
-	        pstmt.setInt(++cnt, num);
-			
+			pstmt.setObject(++cnt, (rank != null) ? rank : 0, Types.INTEGER);
+			pstmt.setObject(++cnt, (volm_cnt != null) ? volm_cnt : 0, Types.INTEGER);
+			pstmt.setInt(++cnt, num);
+
 			int count = pstmt.executeUpdate();
 			System.out.println(count + "개 행의 정보를 수정했습니다.");
 		} catch (Exception e) {
@@ -241,7 +339,7 @@ public class BookDAO_Jw {
 			DBUtil.executeClose(null, pstmt, conn);
 		}
 	} // updateInfo
-	
+
 	// delete
 	public void deleteInfo(int num) {
 		Connection conn = null;
@@ -260,5 +358,5 @@ public class BookDAO_Jw {
 			DBUtil.executeClose(null, pstmt, conn);
 		}
 	} // delete
-	
+
 }
--- Library/src/kr/library/LibraryMain_jw.java
+++ Library/src/kr/library/LibraryMain_jw.java
@@ -4,14 +4,18 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+/**
+ * 처음화면에서 6.반납 / 5.대여예약-2.대여/예약 내역확인 / 149줄?에 추가 이어서 구현  => 3가지 구현
+ * 이후 테스트후 수정
+ */
 
 public class LibraryMain_jw {
 	private BufferedReader br;
-	private String me_id; // 로그인한 아이디 저장
+	private String mem_id; // 로그인한 아이디 저장
 	private boolean isSelectFive = false;
 	private boolean isStart = true;
 	private BookDAO_Jw dao;
-	
+
 	public LibraryMain_jw() {
 		try {
 			dao = new BookDAO_Jw();
@@ -24,10 +28,9 @@ public class LibraryMain_jw {
 			if(br!=null) try {br.close();} catch(IOException e) {}	
 		}
 	}
-	
+
 	// 메뉴 호출
 	private void callMenu() throws IOException {
-		// 로그인 체크 영역
 		while(true) {
 			if(isStart) {
 				System.out.print("5.대여/예약 6.반납 9.종료\n > ");
@@ -53,24 +56,112 @@ public class LibraryMain_jw {
 			}
 			if(isSelectFive) {
 				System.out.print("1.대여/예약하기 2.대여/예약 내역확인 3.뒤로가기\n >");
-				
+				int book_num = 0;
 				try {
 					int no = Integer.parseInt(br.readLine());
-					if(no==1) {
-						isStart = false;
-						isSelectFive = true;
-						
-						dao.randomBookInfo(5);
-						// 책 목록 보여줄 함수 
-						// 책번호 입력받기
-						// 대여 여부(y또는Y)
-						// 대여 시 조건 확인
-						// 책이 없을 시 예약
-						// 예약 시 조건 확인
+					if(no==1) {//완
+						boolean flag = false;
+						boolean isOrder = true;
+						while(isOrder) {
+							System.out.println("랜덤 책 목록");
+							dao.randomBookInfo(5);
+							do {
+								try {
+									if(flag) {
+										System.out.println("존재하지않는 책 번호입니다.");
+									} 
+									System.out.println();
+									System.out.print("대여/예약 하실 책 번호를 입력해주세요 : ");
+									book_num = Integer.parseInt(br.readLine());
+									flag = true;
+								} catch (NumberFormatException e) {
+									System.out.println("[숫자만 입력 가능]");
+								}
+							} while (dao.checkBookNum(book_num) != 1);
 
+							System.out.print("1.대여하기 2.예약하기 3.뒤로가기\n >");
+							try {
+								no = Integer.parseInt(br.readLine());
+								int res = dao.canOrder(mem_id, book_num);
+								if(no==1) {
+									flag = false;
+									if(res == 1) {
+										System.out.println("대여가 가능합니다.");
+										dao.insertBookOrder(mem_id, book_num);
+									}else if(res == 0) {
+										System.out.println("해당 번호의 책의 권수가 0권입니다.");
+										String s;
+										do {
+											if(flag) {
+												System.out.println("Y/N/y/n 중 입력해주세요.");
+											}
+											System.out.print("예약하시겠습니까?(Y/N) : ");
+											s = br.readLine();
+											if(s.equals("N")||s.equals("n")) {
+												System.out.println("예약을 취소하셨습니다.");
+												System.out.println("이전화면으로 돌아갑니다.");
+												isOrder = false;
+												continue;
+											}else if(s.equals("Y")||s.equals("y")) {
+												System.out.println("예약을 진행합니다.");
+												dao.insertReserve(mem_id, book_num);
+											}else {
+												flag = true;
+											}
+										} while (!s.equals("N") && !s.equals("n") && !s.equals("Y") && !s.equals("y"));
+									}else if(res == -1) {
+										System.out.println("대여권수가 이미 3권입니다.");
+										System.out.println("이전화면으로 돌아갑니다.");
+										isOrder = false;
+										continue;
+									}else {
+										System.out.println("프로그램 오류");
+									}
+
+								}else if(no==2) {
+									flag = false;
+									if(res == -1) {
+										System.out.println("대여권수가 이미 3권입니다.");
+										System.out.println("예약이 불가능하니 이전화면으로 돌아갑니다.");
+										isOrder = false;
+										continue;
+									}else if(res == 1) {
+										System.out.println("책 권수가 1권 이상입니다.");
+										String s;
+										do {
+											if(flag) {
+												System.out.println("Y/N/y/n 중 입력해주세요.");
+											}
+											System.out.print("대여하시겠습니까?(Y/N) : ");
+											s = br.readLine();
+											if(s.equals("N")||s.equals("n")) {
+												System.out.println("대여를 취소하셨습니다.");
+												System.out.println("이전화면으로 돌아갑니다.");
+												isOrder = false;
+												continue;
+											}else if(s.equals("Y")||s.equals("y")) {
+												System.out.println("대여를 진행합니다.");
+												dao.insertBookOrder(mem_id, book_num);
+											}else {
+												flag = true;
+											}
+										} while (!s.equals("N") && !s.equals("n") && !s.equals("Y") && !s.equals("y"));
+									}else if(res == 0) {} // 이부분 예약 확인용 함수 만들고 예약권수 찼는지, 책 0권인지 확인 후 나뉘는 함수 작성
+
+								}else if(no==3) {
+									System.out.println("뒤로가기를 선택하셨습니다.");
+									isOrder = false;
+									continue;
+								}else {
+									System.out.println("잘못 입력하셨습니다.");
+								}
+							} catch (NumberFormatException e) {
+								System.out.println("[숫자만 입력 가능]");
+							}
+						}
 					} else if(no==2) {
-						// 반납
-					} else if(no==3) {
+						// 대여/예약 내역확인
+					} else if(no==3) {//완
 						// 뒤로가기
 						System.out.println("뒤로가기를 선택하셨습니다. 홈으로 돌아갑니다.");
 						isStart = true;
@@ -83,9 +174,9 @@ public class LibraryMain_jw {
 				}
 			}
 		}
-		
+
 	}
-	
+
 	public static void main(String[] args) {
 		new LibraryMain_jw();
 	}
