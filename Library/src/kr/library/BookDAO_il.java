2d491c25fede37324627edd7f3e9f9ed9ca0e424
lakky779 <purplelak@gmail.com>
2025-02-20 19:05:38 +0900

    관리자화면 전체메뉴 조회기능 추가 완료(통계 메뉴 제외) 도서관리 세부메뉴 추가중

--- Library/src/kr/library/BookDAO_il.java
+++ Library/src/kr/library/BookDAO_il.java
@@ -14,7 +14,8 @@ import util.DBUtil;
  */
 public class BookDAO_il {
 	//회원 관리
-	public void selectMemberInfo() {
+	//회원 목록보기
+	public void selectMember() {
 		Connection conn = null;
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;		
@@ -30,14 +31,13 @@ public class BookDAO_il {
 			rs = pstmt.executeQuery();
 			System.out.println("-".repeat(100));
 			if (rs.next()) {
-				// PW,정보수정일 제외 출력
-				System.out.println("ID\t이름\t전화번호\t\te-mail\t\t회원가입일\t\t대여정지기한");
+				// 출력항목: ID,이름,전화번호,e-mail,대여정지기한 (나머지는 상세보기에서 확인)
+				System.out.println("ID\t이름\t전화번호\t\te-mail\t\t대여정지기한");
 				do {
 					System.out.print(rs.getString("mem_id")+"\t");
 					System.out.print(rs.getString("mem_name")+"\t");
 					System.out.print(rs.getString("mem_cell")+"\t");	
-					System.out.print(rs.getString("mem_email")+"\t");	
-					System.out.print(rs.getDate("mem_date")+"\t");		
+					System.out.print(rs.getString("mem_email")+"\t");						
 					System.out.println(rs.getDate("mem_stop_date"));
 					//Date mem_stop_date = rs.getDate("mem_stop_date"); // null일 경우 공란 처리 필요
 					//if(mem_stop_date == null) mem_stop_date = ""; // 수정요망
@@ -53,10 +53,102 @@ public class BookDAO_il {
 			// 자원 정리
 			DBUtil.executeClose(rs, pstmt, conn);			
 		} 	
-	} // selectMemberInfo()
+	} // selectMember()
 
-	//도서 관리 -> OK: 도서 목록 조회(20개만) / TODO: 수정,추가,삭제 기능
-	public void selectBookInfo() {
+	//조회하는 회원 레코드 존재 여부 체크
+	public int checkMemberRecord(String mem_id) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String sql = null;		
+		int count = 0;		
+		try {
+			//JDBC 수행 1,2단계
+			conn = DBUtil.getConnection();
+			//SQL문 작성
+			sql = "SELECT * FROM member WHERE mem_id=?";
+			//JDBC 수행 3단계
+			pstmt = conn.prepareStatement(sql);
+			//?에 데이터 바인딩
+			pstmt.setString(1, mem_id);
+			//JDBC 수행 4단계
+			rs = pstmt.executeQuery();
+			if (rs.next()) {
+				count = 1; //레코드가 존재할 때 1 저장				
+			} // if					
+		} catch (Exception e) {
+			count = -1; //오류 발생
+		} finally {
+			//자원정리
+			DBUtil.executeClose(rs, pstmt, conn);			
+		}		
+		return count; 
+	} // checkMemberRecord()
+
+	//회원 상세정보 확인
+	public void selectDetailMember(String mem_id) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String sql = null;
+		try {
+			//JDBC 수행 1,2단계
+			conn = DBUtil.getConnection();
+			//SQL문 작성
+			sql = "SELECT * FROM member WHERE mem_id=?";
+			//JDBC 수행 3단계 
+			pstmt = conn.prepareStatement(sql);
+			//?에 데이터 할당
+			pstmt.setString(1, mem_id);
+			//JDBC 수행 4단계
+			rs = pstmt.executeQuery();
+			if (rs.next()) {
+				// 비밀번호만 미출력
+				System.out.println("회원ID : " + rs.getString("mem_id"));
+				System.out.println("이름 : " + rs.getString("mem_name"));	
+				System.out.println("전화번호 : " + rs.getString("mem_cell"));						
+				System.out.println("이메일 : " + rs.getString("mem_email"));
+				System.out.println("가입일 : " + rs.getDate("mem_date"));
+				System.out.println("마지막 정보수정일 : " + rs.getDate("mem_mdate"));
+				System.out.println("정지일 : " + rs.getDate("mem_stop_date"));
+			} else {
+				System.out.println("검색된 정보가 없습니다.");
+			}			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			//자원정리
+			DBUtil.executeClose(rs, pstmt, conn);			
+		}		
+	} // selectDetailMembe
+
+	// 회원 정보 삭제
+	public void deleteMember(String mem_id) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		String sql = null;
+		try {
+			//JDBC 수행 1,2단계
+			conn = DBUtil.getConnection();
+			//SQL문 작성
+			sql = "DELETE FROM member WHERE mem_id=?";
+			//JDBC 수행 3단계
+			pstmt = conn.prepareStatement(sql);
+			//?에 데이터 바인딩
+			pstmt.setString(1, mem_id);
+			//JDBC 수행 4단계
+			int count = pstmt.executeUpdate();
+			System.out.println(count + "개의 회원정보를 삭제했습니다.");
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			//자원정리
+			DBUtil.executeClose(null, pstmt, conn);
+		}
+	} // deleteMember()
+
+	//도서 관리 -> OK: 도서 목록 조회(우선 20개만) / TODO: 상세보기,등록,수정,삭제 기능
+	public void selectBook() {
 		Connection conn = null;
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;		
@@ -94,10 +186,113 @@ public class BookDAO_il {
 			// 자원 정리
 			DBUtil.executeClose(rs, pstmt, conn);			
 		} 	
-	} // selectBookInfo()
+	} // selectBook()
+
+	//조회하는 책 레코드 존재 여부 체크
+	public int checkBookRecord(int book_num) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String sql = null;		
+		int count = 0;		
+		try {
+			//JDBC 수행 1,2단계
+			conn = DBUtil.getConnection();
+			//SQL문 작성
+			sql = "SELECT * FROM book WHERE book_num=?";
+			//JDBC 수행 3단계
+			pstmt = conn.prepareStatement(sql);
+			//?에 데이터 바인딩
+			pstmt.setInt(1, book_num);
+			//JDBC 수행 4단계
+			rs = pstmt.executeQuery();
+			if (rs.next()) {
+				count = 1; //레코드가 존재할 때 1 저장				
+			} // if					
+		} catch (Exception e) {
+			count = -1; //오류 발생
+		} finally {
+			//자원정리
+			DBUtil.executeClose(rs, pstmt, conn);			
+		}		
+		return count; 
+	} // checkBookRecord()
+		
+	//책 정보 상세보기
+		public void selectDetailBook(int book_num) {
+			Connection conn = null;
+			PreparedStatement pstmt = null;
+			ResultSet rs = null;
+			String sql = null;
+			try {
+				//JDBC 수행 1,2단계
+				conn = DBUtil.getConnection();
+				//SQL문 작성
+				sql = "SELECT * FROM book WHERE book_num=?";
+				//JDBC 수행 3단계 
+				pstmt = conn.prepareStatement(sql);
+				//?에 데이터 할당
+				pstmt.setInt(1, book_num);
+				//JDBC 수행 4단계
+				rs = pstmt.executeQuery();
+				if (rs.next()) {
+					System.out.println("책번호 : " + rs.getInt("book_num"));
+					System.out.println("제목 : " + rs.getString("book_title"));
+					System.out.println("저자" + rs.getString("book_author"));
+					System.out.println("출판사 : " + rs.getString("book_publisher"));
+					System.out.println("출판년도 : " + rs.getInt("book_p_year"));
+					System.out.println("카테고리 : " + rs.getString("book_category"));
+					System.out.println("추천순위 : " + rs.getInt("book_rank"));
+					System.out.println("보유권수 : " + rs.getInt("book_volm_cnt"));
+					System.out.println("등록일 : " + rs.getDate("book_reg_date"));		
+				} else {
+					System.out.println("검색된 정보가 없습니다.");
+				}			
+			} catch (Exception e) {
+				e.printStackTrace();
+			} finally {
+				//자원정리
+				DBUtil.executeClose(rs, pstmt, conn);	
+			}		
+		} // selectDetailBook()
+	
+	//책 정보 등록
+	public void insertBook(String book_title, String book_author, String book_publisher, 
+			int book_p_year, String book_category, int book_rank, int book_volm_cnt) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		String sql = null;
+		int cnt = 0;		
+		try {
+			//JDBC 수행 1,2단계
+			conn = DBUtil.getConnection();
+			//SQL문 작성
+			sql = "INSERT INTO book (book_num, book_title, book_author, book_publisher, "
+					+ "book_p_year, book_category, book_rank, book_volm_cnt, book_reg_date) "
+					+ "VALUES (book_seq.nextval,?,?,?,?,?,?,?,SYSDATE)"; //전체를 넣을땐 컬럼명 생략 가능
+			//JDBC 수행 3단계
+			pstmt = conn.prepareStatement(sql);
+			//?에 데이터 바인딩
+			pstmt.setString(++cnt, book_title);
+			pstmt.setString(++cnt, book_author);
+			pstmt.setString(++cnt, book_publisher);
+			pstmt.setInt(++cnt, book_p_year);	
+			pstmt.setString(++cnt, book_category);
+			pstmt.setInt(++cnt, book_rank);	
+			pstmt.setInt(++cnt, book_volm_cnt);				
+			//JDBC 수행 4단계
+			int count = pstmt.executeUpdate();			
+			System.out.println(count + "개의 행을 삽입했습니다.");			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			//자원정리
+			DBUtil.executeClose(null, pstmt, conn);			
+		} 		
+	} // insertBook()
 
 	// 대여 관리
-	public void selectOrderInfo() {
+	public void selectOrder() {
 		Connection conn = null;
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;		
@@ -135,10 +330,10 @@ public class BookDAO_il {
 			// 자원 정리
 			DBUtil.executeClose(rs, pstmt, conn);			
 		} 	
-	} // selectOrderInfo()
+	} // selectOrder()
 
 	// 예약 관리
-	public void selectRSVInfo() {
+	public void selectRSV() {
 		Connection conn = null;
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;		
@@ -147,7 +342,7 @@ public class BookDAO_il {
 			//JDBC 수행 1,2단계
 			conn = DBUtil.getConnection();
 			//SQL문 작성
-			sql = "SELECT * FROM reservation ORDER BY re_num";
+			sql = "SELECT * FROM reservation ORDER BY book_num, re_num"; // 1.책번호별 2.예약번호별 정렬 (test요망)
 			//JDBC 수행 3단계
 			pstmt = conn.prepareStatement(sql);
 			//JDBC 수행 4단계
@@ -170,10 +365,10 @@ public class BookDAO_il {
 			// 자원 정리
 			DBUtil.executeClose(rs, pstmt, conn);			
 		} 	
-	} // selectRSVInfo()
+	} // selectRSV()
 
 	// 희망도서 관리
-	public void selectWishInfo() {
+	public void selectWish() {
 		Connection conn = null;
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;		
@@ -207,7 +402,7 @@ public class BookDAO_il {
 			// 자원 정리
 			DBUtil.executeClose(rs, pstmt, conn);			
 		} 	
-	} // selectWishInfo()
+	} // selectWish()
 
 	// 리뷰 관리
 	public void selectReview() {
@@ -225,13 +420,7 @@ public class BookDAO_il {
 			//JDBC 수행 4단계
 			rs = pstmt.executeQuery();
 			System.out.println("-".repeat(100));
-			if (rs.next()) {	
-//				review_num            리뷰번호
-//				book_num              책번호
-//				review_content        리뷰내용
-//				review_rate           평점
-//				review_reg_date       리뷰등록일
-//				mem_id				  회원 아이디
+			if (rs.next()) {
 				System.out.println("리뷰번호\t책번호\t리뷰내용\t평점\t리뷰등록일\t회원아이디");			
 				do {
 					System.out.print(rs.getInt("review_num")+"\t");							
@@ -252,6 +441,117 @@ public class BookDAO_il {
 			DBUtil.executeClose(rs, pstmt, conn);			
 		} 	
 	} // selectReview()
+
+	// 공지사항 관리
+	public void selectNotice() {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;		
+		String sql = null;
+		try {
+			//JDBC 수행 1,2단계
+			conn = DBUtil.getConnection();
+			//SQL문 작성
+			sql = "SELECT * FROM notice ORDER BY notice_num DESC";
+			//JDBC 수행 3단계
+			pstmt = conn.prepareStatement(sql);
+			//JDBC 수행 4단계
+			rs = pstmt.executeQuery();
+			System.out.println("-".repeat(100));
+			if (rs.next()) {			
+				System.out.println("공지사항번호\t공지사항제목\t공지사항내용\t\t공지사항조회수\t공지사항등록일");		
+				do {
+					System.out.print(rs.getInt("notice_num")+"\t\t");							
+					System.out.print(rs.getString("notice_title")+"\t");
+					System.out.print(rs.getString("notice_content")+"\t");
+					System.out.print(rs.getInt("notice_view")+"\t\t");					
+					System.out.println(rs.getDate("notice_reg_date")+"\t");
+				} while (rs.next());
+			} else {
+				System.out.println("표시할 데이터가 없습니다.");	
+			} //if-else
+			System.out.println("-".repeat(100));
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			// 자원 정리
+			DBUtil.executeClose(rs, pstmt, conn);			
+		} 	
+	} // selectNotice()
+
+	// Q&A 관리
+	public void selectQnA() {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;		
+		String sql = null;
+		try {
+			//JDBC 수행 1,2단계
+			conn = DBUtil.getConnection();
+			//SQL문 작성
+			sql = "SELECT * FROM qna ORDER BY qna_num"; // 수정요망
+			//JDBC 수행 3단계
+			pstmt = conn.prepareStatement(sql);
+			//JDBC 수행 4단계
+			rs = pstmt.executeQuery();
+			System.out.println("-".repeat(100));
+			if (rs.next()) {
+				System.out.println("qna번호\t질문제목\t질문내용\t조회수\t답변내용\t질문날짜\t\t답변날짜");		
+				do {
+					System.out.print(rs.getInt("qna_num")+"\t");							
+					System.out.print(rs.getString("qna_title")+"\t");
+					System.out.print(rs.getString("qna_content")+"\t");
+					System.out.print(rs.getInt("qna_view")+"\t");	
+					System.out.print(rs.getString("qna_re")+"\t");	
+					System.out.print(rs.getDate("q_date")+"\t");	
+					System.out.println(rs.getDate("a_date")+"\t");
+				} while (rs.next());
+			} else {
+				System.out.println("표시할 데이터가 없습니다.");	
+			} //if-else
+			System.out.println("-".repeat(100));
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			// 자원 정리
+			DBUtil.executeClose(rs, pstmt, conn);			
+		} 	
+	} // selectQnA()
+
+	// 통계 관리
+	public void selectStatistics() {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;		
+		String sql = null;
+		try {
+			//JDBC 수행 1,2단계
+			conn = DBUtil.getConnection();
+			//SQL문 작성
+			sql = "SELECT * FROM qna ORDER BY qna_num"; // 수정요망!!!!!
+			//JDBC 수행 3단계
+			pstmt = conn.prepareStatement(sql);
+			//JDBC 수행 4단계
+			rs = pstmt.executeQuery();
+			System.out.println("-".repeat(100));
+			if (rs.next()) {
+				System.out.println("항목출력\t");		
+				do {
+					//					System.out.print(rs.getInt("")+"\t");						
+					//					:
+					//					System.out.println(rs.getDate("")+"\t");
+				} while (rs.next());
+			} else {
+				System.out.println("표시할 데이터가 없습니다.");
+			} //if-else
+			System.out.println("-".repeat(100));
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			// 자원 정리
+			DBUtil.executeClose(rs, pstmt, conn);			
+		} 	
+	} // selectStatistics()
 
 	/*
 	//관리자(admin) 로그인 체크(로그인 성공 true, 로그인 실패 false 반환)
--- Library/src/kr/library/LibraryMain_il.java
+++ Library/src/kr/library/LibraryMain_il.java
@@ -38,23 +38,90 @@ public class LibraryMain_il {
 					+ "번호를 입력하세요.> ");
 			try {
 				int no = Integer.parseInt(br.readLine());
-				if (no==1) dao.selectMemberInfo(); // 회원 관리				
-				else if (no==2) { // 도서 관리			
-					dao.selectBookInfo(); // 수정,추가,삭제 기능 추가요망
+				if (no==1) { // 회원 관리
+					System.out.print("1.회원 목록보기, 2.회원 상세정보 확인, 3.회원정보 삭제\n"
+							+ "번호를 입력하세요.> ");
+					int num = Integer.parseInt(br.readLine());				
+					if (num==1) { // 1-1.회원 목록보기
+						// 목록보기
+						dao.selectMember();
+					} else if (num==2) { // 1-2.회원 상세정보 확인
+						dao.selectMember();		
+						System.out.print("조회할 회원ID 입력: ");
+						String mem_id = br.readLine();
+						int count = dao.checkMemberRecord(mem_id);
+						//잘못 입력하면 다시 입력받음
+						do {
+							if (count==0) {
+								System.out.print("회원ID를 잘못 입력했습니다. 다시입력하세요.: ");
+								mem_id = br.readLine();
+								count = dao.checkMemberRecord(mem_id);
+							} else if (count!=1) {
+								System.out.println("정보 처리 중 오류 발생");
+							} // if
+						} while (count!=1);
+						dao.selectDetailMember(mem_id);
+					} else if (num==3) { // 1-3.회원 삭제
+						dao.selectMember();
+						System.out.print("삭제할 회원ID 입력: ");
+						String mem_id = br.readLine();
+						int count = dao.checkMemberRecord(mem_id);
+						//잘못 입력하면 다시 입력받음
+						do {
+							if (count==0) {
+								System.out.print("회원ID를 잘못 입력했습니다. 다시입력하세요.: ");
+								mem_id = br.readLine();
+								count = dao.checkMemberRecord(mem_id);
+							} else if (count!=1) {
+								System.out.println("정보 처리 중 오류 발생");
+							} // if
+						} while (count!=1);
+						dao.deleteMember(mem_id);
+					}
+
+				} else if (no==2) { // 도서 관리			
+					// dao.selectBook();
+					// 수정,등록,삭제 기능 추가요망
+					System.out.print("1.도서 목록보기, 2.도서 상세정보 확인, 3.도서정보 등록, 4.도서정보 수정, 5.도서정보 삭제\n"
+							+ "번호를 입력하세요.> ");
+					int num = Integer.parseInt(br.readLine());				
+					if (num==1) { // 2-1.도서 목록보기
+						// 목록보기
+						dao.selectBook();
+					} else if (num==2) { // 2-2.도서 상세정보 확인
+						dao.selectBook();		
+						System.out.print("조회할 책번호 입력: ");
+						int book_num = Integer.parseInt(br.readLine());
+						int count = dao.checkBookRecord(book_num);
+						//잘못 입력하면 다시 입력받음
+						do {
+							if (count==0) {
+								System.out.print("책번호를 잘못 입력했습니다. 다시입력하세요.: ");
+								mem_id = br.readLine();
+								count = dao.checkBookRecord(book_num);
+							} else if (count!=1) {
+								System.out.println("정보 처리 중 오류 발생");
+							} // if
+						} while (count!=1);
+						dao.selectDetailBook(book_num);
+					} else if (num==3) { //2-3.도서정보 등록	
+						//추가중~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~			
+					} // 2-4.도서정보 수정, 2-5.도서정보 삭제 추가 예정
+
 				} else if (no==3) { // 대여 관리				
-					dao.selectOrderInfo();
+					dao.selectOrder();
 				} else if (no==4) { // 예약 관리
-					dao.selectRSVInfo();
+					dao.selectRSV();
 				} else if (no==5) { // 희망도서 관리
-					dao.selectWishInfo();
+					dao.selectWish();
 				} else if (no==6) { // 리뷰 관리
 					dao.selectReview();
 				} else if (no==7) { // 공지사항 관리
-					System.out.println("공지사항");					
+					dao.selectNotice();
 				} else if (no==8) { // Q&A 관리
-					System.out.println("Q&A 관리");
+					dao.selectQnA();
 				} else if (no==9) { // 통계 관리
-					System.out.println("통계 관리");
+					System.out.println("통계 관리: 작성 요망");
 				} else if (no==10) { // 프로그램 종료 
 					System.out.println("프로그램 종료");
 					break;
